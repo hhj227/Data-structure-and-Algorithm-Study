4270a26d462f947928b0f48d480fbdbfa2f590de
Hyejung Han <gamcho227@naver.com>
2019-08-24 21:46:49 +0900

    Create 네트워크.cpp

---
+++ programmers/DFS:BFS/네트워크.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <cstring>
+using namespace std;
+
+int solution(int n, vector<vector<int>> computers) {
+    int answer = 0;
+    bool visited[200];
+    memset(visited, false, sizeof(visited));
+    queue<int> q;
+    for(int i=0;i<n;i++){
+        if(visited[i]) continue;
+        q.push(i);
+        answer++;
+        while(!q.empty()){
+            int idx = q.front();
+            visited[idx] = true;
+            q.pop();
+            for(int j=0;j<n;j++){
+                if(computers[idx][j]==1&&!visited[j]){
+                    q.push(j);
+                }
+            }
+        }
+    }
+    
+    
+    return answer;
+}
