e065977978570e293f91a5bafac834a77a367686
Hyejung Han <gamcho227@naver.com>
2019-08-24 17:47:50 +0900

    Create 전화번호 목록.py

---
+++ programmers/해시/전화번호 목록.py
@@ -0,0 +1,14 @@
+# first solution
+
+def solution(phone_book):
+    answer = True
+    
+    for a in range(len(phone_book)):
+        for b in range(len(phone_book)):
+            if a==b or len(phone_book[a])>len(phone_book[b]):
+                continue
+            if phone_book[b][0:len(phone_book[a])]==phone_book[a]:
+                answer = False
+                break
+    
+    return answer
