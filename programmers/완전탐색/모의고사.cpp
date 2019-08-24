7d4ce052e6aaaa1515a69cc5f4d449d3d7a47887
hhj227 <gamcho227@naver.com>
2019-08-24 17:05:50 +0900

    first commit

---
+++ programmers/완전탐색/모의고사.cpp
@@ -0,0 +1,12 @@
+def solution(answers):
+    answer = []
+    p1 = [1,2,3,4,5]
+    p2 = [2,1,2,3,2,4,2,5]
+    p3 = [3,3,1,1,2,2,4,4,5,5]
+    
+    a1 = a2 = a3 = 0
+    
+    for i in range(len(answers)):
+        print(i)
+    
+    return answer
